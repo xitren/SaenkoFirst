5eb082b1fe4bc9e7ba4ed2e72973acdc703c962e
Максим <xerokan@mail.ru>
2021-03-22 22:55:56 +0300

    Json+names

--- src/main/java/com/gusev/fx/UpperController.java
+++ src/main/java/com/gusev/fx/UpperController.java
@@ -1,5 +1,7 @@
 package com.gusev.fx;
 
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.application.Application;
 import javafx.scene.Group;
@@ -8,10 +10,13 @@ import javafx.stage.Stage;
 import javafx.scene.shape.Circle;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.Pane;
-
+import javafx.scene.layout.StackPane;
+import org.aspectj.weaver.bcel.BcelRenderer;
+import org.dom4j.Text;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -38,27 +43,29 @@ public class UpperController implements Initializable {
 
         JSONParser parser = new JSONParser();
 
-        try (Reader reader = new FileReader("/home/maxim/graf.json")) {
+        try (Reader reader = new FileReader("graf.json")) {
 
             JSONObject jsonObject = (JSONObject) parser.parse(reader);
             System.out.println(jsonObject);
-
             JSONArray nodes = (JSONArray) jsonObject.get("nodes");
-            nodes.forEach((obj)->{
+            nodes.forEach((obj) -> {
                 JSONObject node = (JSONObject) obj;
                 long Xcenter = (long) node.get("centerx");
                 long Ycenter = (long) node.get("centery");
                 long r = (long) node.get("r");
-                System.out.println(" "+r+" "+Ycenter+" "+Xcenter);
-                view.getChildren().add(new Circle(Xcenter, Ycenter,r));
+                String name = (String) node.get("name");
+                System.out.println(" " + r + " " + Ycenter + " " + Xcenter);
+                StackPane stack = new StackPane();
+                Circle circle = new Circle(Xcenter, Ycenter, r, Color.RED);
+                Label label = new Label(name);
+                stack.getChildren().addAll(circle, label);
+                StackPane.setAlignment(circle, Pos.TOP_CENTER);
+                StackPane.setAlignment(label, Pos.TOP_CENTER);
+                StackPane.setMargin(circle, new Insets(circle.getCenterY(), 0, 0, circle.getCenterX()));
+                StackPane.setMargin(label, new Insets(circle.getCenterY(), 0, 0, circle.getCenterX()));
+                view.getChildren().add(stack);
+                view.getChildren().add(stack);
             });
-
-            String shopperName = (String) jsonObject.get("shopperName");
-            System.out.println(shopperName);
-
-            String shopperEmail = (String) jsonObject.get("shopperEmail");
-            System.out.println(shopperEmail);
-
 
         }catch (IOException c) {
             c.printStackTrace();
